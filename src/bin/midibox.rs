d1f8f95470659d9ca499a8ed700535d2b429c9b5
Alex Pagan <alexrpagan@gmail.com>
2023-01-21 15:45:33 -0500

    Add `gate` function that combines `flatten` and `mask`

--- src/bin/midibox.rs
+++ src/bin/midibox.rs
@@ -1,5 +1,5 @@
 use std::sync::Arc;
-use musicbox::{Degree, FixedSequence, Midibox, run, Scale, Tone};
+use musicbox::{Degree, FixedSequence, Midibox, Scale, Tone, run};
 use musicbox::Interval::Oct;
 
 fn main() {
@@ -24,25 +24,25 @@ fn main() {
         Tone::C.oct(4)  * 16,
     ]);
 
-    match run(300, vec![
+    run(300, vec![
         s1.clone()
             .velocity(70)
-            .transpose_down(Oct).flatten().mask(vec![true, false, true, true, false, true, false, true]),
+            .transpose_down(Oct).gate(vec![true, false, true, true, false, true, false, true]),
         s1.clone()
             .velocity(60)
             .transpose_down(Oct)
             .harmonize_down(&c_maj, Degree::Fourth)
-            .flatten().mask(vec![false, true, false, false, true, false, true, false]),
+            .gate(vec![false, true, false, false, true, false, true, false]),
         s1.clone()
-            .flatten().mask(vec![true, false, false, true]),
+            .gate(vec![true, false, false, true]),
         s1.clone()
             .velocity(110)
             .harmonize_up(&c_maj, Degree::Third)
-            .flatten().mask(vec![false, true, false, false, true]),
+            .gate(vec![false, true, false, false, true]),
         s1.clone()
             .velocity(60)
             .harmonize_down(&c_maj, Degree::Second)
-            .flatten().mask(vec![false, false, true, false, false, true]),
+            .gate(vec![false, false, true, false, false, true]),
         s1.clone()
             .velocity(90)
             .harmonize_up(&c_maj, Degree::Fifth)
@@ -50,9 +50,7 @@ fn main() {
         s1.clone()
             .velocity(80)
             .harmonize_down(&c_maj, Degree::Fourth)
-            .flatten().mask(vec![false, false, true, true, false, false, true]),
-    ].into_iter().map(|v| -> Arc<dyn Midibox> { Arc::new(v) }).collect()) {
-        Ok(_) => (),
-        Err(err) => println!("Error: {}", err)
-    }
+            .gate(vec![false, false, true, true, false, false, true]),
+    ].into_iter().map(|v| -> Arc<dyn Midibox> { Arc::new(v) }).collect()
+    )
 }
