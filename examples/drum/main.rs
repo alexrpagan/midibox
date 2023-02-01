f2630d990e4e01fa5af4fec3d6c432145e7c0836
Alex Pagan <alexrpagan@gmail.com>
2023-01-31 23:19:14 -0500

    Add polyrhythmic drum example

---
+++ examples/drum/main.rs
@@ -0,0 +1,55 @@
+use midibox::{Bpm, Degree, Interval, Scale, Tone};
+use midibox::sequences::FixedSequence;
+use midibox::player::run;
+
+// preset 494, 21
+fn main() {
+    let _scale = Scale::major(Tone::B);
+
+    let roots = FixedSequence::new(vec![
+        Tone::C.oct(3),
+        Tone::G.oct(3),
+        Tone::E.oct(2),
+        Tone::B.oct(3),
+        Tone::D.oct(3),
+        Tone::B.oct(2),
+        Tone::G.oct(2),
+        Tone::D.oct(4),
+        Tone::C.oct(3),
+        Tone::E.oct(3),
+        Tone::G.oct(2),
+        Tone::B.oct(3),
+        Tone::D.oct(3),
+        Tone::B.oct(2),
+        Tone::G.oct(2),
+        Tone::B.oct(2)
+    ]).transpose_down(Interval::Min2);
+
+    let fast = roots.clone().duration(2).split_notes(vec![true, false]).repeat(5);
+    let slow = roots.clone().duration(5).repeat(2);
+    let slow_ff1 = roots.clone().duration(5).fast_forward(1).repeat(2);
+
+    run(
+        Bpm::new(500),
+        vec![
+            (
+                fast.clone()
+                    + fast.clone().transpose_down(Interval::Perf4)
+                    + fast.clone()
+                    + fast.clone().transpose_down(Interval::Min3)
+            ).midibox(),
+            (
+                slow_ff1.clone()
+                    .split_notes(vec![true, false, false])
+                    + slow_ff1.clone()
+                    .split_notes(vec![false, true, false, false, true])
+                    .transpose_down(Interval::Perf4)
+                    + slow.clone()
+                    .split_notes(vec![true, false, false])
+                    + slow.clone()
+                    .split_notes(vec![false, true, false, false, true])
+                    .transpose_down(Interval::Min3)
+            ).midibox()
+        ]
+    )
+}
