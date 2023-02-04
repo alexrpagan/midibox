9fd58ce31f7b91ba9e45b845e87253d318f961de
Alex Pagan <alexrpagan@gmail.com>
2023-02-03 21:36:46 -0500

    Add ToMidi trait

--- examples/drumlogue/main.rs
+++ examples/drumlogue/main.rs
@@ -1,93 +1,63 @@
-use Drumlogue::{BD, CH, CP, HT, LT, OH, RS, SD, SP1, SP2};
-use midibox::{Bpm, Degree, Interval, Scale, Tone, Midi};
+use midibox::Bpm;
+use midibox::drumlogue::Drumlogue::{BD, CH, CP, HT, LT, OH, RS, SD};
 use midibox::sequences::FixedSequence;
 use midibox::player::run;
 use midibox::Tone::Rest;
-
-enum Drumlogue {
-    BD,
-    SD,
-    LT,
-    HT,
-    CH,
-    OH,
-    RS,
-    CP,
-    SP1,
-    SP2,
-}
-
-impl Drumlogue {
-    pub fn tone(&self) -> Midi {
-        match self {
-            BD => { Tone::C.oct(2) }
-            SD => { Tone::E.oct(2) }
-            LT => { Tone::A.oct(2) }
-            HT => { Tone::D.oct(3) }
-            CH => { Tone::Gb.oct(2) }
-            OH => { Tone::Bb.oct(2) }
-            RS => { Tone::Db.oct(2) }
-            CP => { Tone::Eb.oct(2) }
-            SP1 => { Tone::E.oct(3) }
-            SP2 => { Tone::F.oct(3) }
-        }
-    }
-}
+use midibox::ToMidi;
 
 fn main() {
     run(
         Bpm::new(1800),
         vec![
             FixedSequence::new(vec![
-                RS.tone().set_velocity(30) * 4,
-                Rest.get() * 8,
-                Rest.get() * 16
+                RS.midi().set_velocity(30) * 4,
+                Rest.midi() * 8,
+                Rest.midi() * 16
             ]),
             FixedSequence::new(vec![
-                Rest.get() * 4,
-                CH.tone().set_velocity(15) * 8
+                Rest.midi() * 4,
+                CH.midi().set_velocity(15) * 8
             ]),
             FixedSequence::new(vec![
-                BD.tone().set_velocity(100) * 16,
-                BD.tone().set_velocity(70) * 16,
-                BD.tone().set_velocity(50) * 16,
-                BD.tone().set_velocity(70) * 16,
-                BD.tone() * 16,
-                BD.tone() * 16,
-                BD.tone() * 16,
-                BD.tone() * 8,
-                BD.tone().set_velocity(15) * 4,
-                BD.tone().set_velocity(20) * 4,
+                BD.midi().set_velocity(100) * 32,
+                BD.midi().set_velocity(50) * 4,
+                BD.midi().set_velocity(70) * 4,
+                Rest.midi() * 28,
+                BD.midi() * 32,
+                BD.midi() * 16,
+                BD.midi() * 8,
+                BD.midi().set_velocity(15) * 4,
+                BD.midi().set_velocity(20) * 4,
             ]),
             FixedSequence::new(vec![
-                Rest.get() * 16,
-                SD.tone() * 16,
+                Rest.midi() * 16,
+                SD.midi() * 16,
             ]),
             FixedSequence::new(vec![
-                Rest.get() * 48,
-                CP.tone() * 16,
+                Rest.midi() * 48,
+                CP.midi() * 16,
             ]),
             FixedSequence::new(vec![
-                Rest.get() * 32,
-                HT.tone().set_velocity(30) * 4,
-                LT.tone().set_velocity(70) * 4,
-                Rest.get() * 16
+                Rest.midi() * 32,
+                HT.midi().set_velocity(30) * 4,
+                LT.midi().set_velocity(70) * 4,
+                Rest.midi() * 16
             ]),
             FixedSequence::new(vec![
-                Rest.get() * 32,
-                Rest.get() * 8,
-                BD.tone().set_velocity(30) * 4,
-                BD.tone().set_velocity(100) * 4,
-                Rest.get() * 8
+                Rest.midi() * 32,
+                Rest.midi() * 8,
+                BD.midi().set_velocity(30) * 4,
+                BD.midi().set_velocity(100) * 4,
+                Rest.midi() * 8
             ]),
             FixedSequence::new(vec![
-                Rest.get() * 8,
-                CH.tone().set_velocity(40) * 8,
-                Rest.get() * 8,
-                CH.tone().set_velocity(44) * 8,
-                Rest.get() * 8,
-                OH.tone().set_velocity(42) * 4,
-                CH.tone().set_velocity(43) * 4,
+                Rest.midi() * 8,
+                CH.midi().set_velocity(40) * 8,
+                Rest.midi() * 8,
+                CH.midi().set_velocity(44) * 8,
+                Rest.midi() * 8,
+                OH.midi().set_velocity(42) * 4,
+                CH.midi().set_velocity(43) * 4,
             ]),
         ].into_iter().map(|seq| seq.midibox()).collect()
     )
