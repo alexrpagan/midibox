df1c2add54d2de96ff6a8ecd0832ffc93ddbd339
Alexander Pagan <alex@alexanders-mbp.lan>
2023-02-25 19:13:40 -0500

    Update examples

--- examples/random/main.rs
+++ examples/random/main.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
 use midibox::{Bpm, Degree, Interval, Scale, ToMidi, Tone};
-use midibox::drumlogue::Drumlogue::{BD, CH, OH};
+use midibox::drumlogue::Drumlogue::{BD, CH, CP, OH, SP1};
 use midibox::sequences::Seq;
 use midibox::rand::RandomVelocity;
 use midibox::player::{PlayerConfig, try_run};
 use midibox::router::MapRouter;
+use midibox::Tone::Rest;
 
 
 fn main() {
@@ -12,39 +13,74 @@ fn main() {
 
     let drums = vec![
         RandomVelocity::wrap(Seq::new(vec![
-            CH.midi().set_velocity(100) * 1,
-            CH.midi().set_velocity(100) * 1,
-            OH.midi().set_velocity(100) * 1,
-            CH.midi().set_velocity(100) * 1,
+            CH.midi() * 1,
+            CH.midi() * 1,
+            OH.midi() * 1,
+            CH.midi() * 1,
+            CH.midi() * 1,
+            CH.midi() * 1,
+            CH.midi() * 1,
+            OH.midi() * 1,
         ]).midibox()),
         Seq::new(vec![
-            BD.midi() * 4
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 1,
+            BD.midi() * 3,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 4,
+            BD.midi() * 1,
+            BD.midi() * 2,
+            BD.midi() * 1,
+        ]).midibox(),
+        Seq::new(vec![
+            Rest.midi() * 4,
+            CP.midi() * 4
+        ]).midibox(),
+        Seq::new(vec![
+            SP1.midi() * 1,
+            Rest.midi() * 3,
+            SP1.midi() * 1
         ]).midibox()
     ];
-    for i in 0..drums.len() {
-        channel_id_to_port_id.insert(i, 1);
-    }
 
     let scale = Scale::major(Tone::Gb);
 
     let roots = Seq::new(vec![
-        Tone::G.oct(2)  * 128,
-        Tone::B.oct(2)  * 128,
-        Tone::E.oct(2)  * 128,
-        Tone::D.oct(2)  * 128,
-        Tone::C.oct(2)  * 128,
-        Tone::E.oct(2)  * 128,
-        Tone::B.oct(2)  * 128,
-        Tone::C.oct(2)  * 128,
+        Tone::G.oct(2)  * 96,
+        Tone::B.oct(2)  * 64,
+        Tone::E.oct(2)  * 96,
+        Tone::D.oct(2)  * 64,
+        Tone::C.oct(2)  * 96,
+        Tone::E.oct(2)  * 64,
+        Tone::B.oct(2)  * 96,
+        Tone::C.oct(2)  * 64,
     ]).transpose_down(Interval::Min2);
 
     let synth: Vec<_> = vec![
-        roots.clone().split_notes(vec![true, false, false]),
-        roots.clone().harmonize_down(&scale, Degree::Fourth).split_notes(vec![false, true, false]),
-        roots.clone().harmonize_up(&scale, Degree::Tenth).split_notes(vec![true, false, true]),
-        roots.clone().harmonize_up(&scale, Degree::Seventh).split_notes(vec![false, false, false, true]),
+        roots.clone()
+            .split_notes(vec![true, false, false]),
+        roots.clone().harmonize_down(&scale, Degree::Fourth)
+            .split_notes(vec![false, true, false]),
+        roots.clone().harmonize_up(&scale, Degree::Tenth)
+            .split_notes(vec![true, false, true]),
+        roots.clone().harmonize_up(&scale, Degree::Seventh)
+            .split_notes(vec![false, false, false, true]),
     ].into_iter().map(|seq| RandomVelocity::wrap(seq.midibox())).collect();
 
+    for i in 0..drums.len() {
+        channel_id_to_port_id.insert(i, 1);
+    }
     for i in drums.len()..(drums.len() + synth.len()) {
         channel_id_to_port_id.insert(i, 0);
     }
