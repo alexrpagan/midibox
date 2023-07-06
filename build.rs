5bec4eb516142fbeda2ee8006e9685ec3213be1a
Sam Roberts <samgqroberts@gmail.com>
2023-07-06 18:40:40 -0400

    fix: failure in build script if output dir does not exist
    create dir if doesn't exist

--- build.rs
+++ build.rs
@@ -1,10 +1,14 @@
 use std::env;
+use std::fs::create_dir_all;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let original_out_dir = PathBuf::from(env::var("OUT_DIR")?);
 
+    // create output directory if doesn't exist
     let out_dir = "./src/generated";
+    create_dir_all(out_dir)?;
+
     tonic_build::configure()
         .build_server(true)
         .out_dir(out_dir)
