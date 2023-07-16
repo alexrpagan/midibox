8834f9423e555acd625c032674fb383f84b8c306
Alex Pagan <alexrpagan@users.noreply.github.com>
2023-07-16 11:41:36 -0400

    Add custom order arpeggios

--- examples/arp/main.rs
+++ examples/arp/main.rs
@@ -12,37 +12,36 @@ use midibox::tone::Tone;
 
 
 fn main() {
+    env_logger::init();
+
     let s1 = seq![
         chord![
-            Tone::G.oct(2) * 128,
+            Tone::C.oct(3) * 128,
             Tone::E.oct(3),
-            Tone::B.oct(3)
+            Tone::B.oct(3),
+            Tone::D.oct(4),
+            Tone::Gb.oct(5)
         ].velocity(70),
 
         chord![
-            Tone::C.oct(2) * 128,
+            Tone::A.oct(2) * 128,
+            Tone::C.oct(3),
             Tone::E.oct(3),
-            Tone::A.oct(3)
-        ].velocity(40),
-
-        chord![
-            Tone::D.oct(3) * 128,
-            Tone::Gb.oct(3),
-            Tone::A.oct(3)
-        ].velocity(20),
-
-        chord![
-            Tone::D.oct(3) * 128,
-            Tone::Gb.oct(3),
+            Tone::G.oct(3),
             Tone::B.oct(3)
-        ].velocity(50)
+        ].velocity(40)
     ];
 
     try_run(
-        PlayerConfig::for_port(2),
+        PlayerConfig::for_port(0),
         &Bpm::new(2000),
         &mut vec![
-            Arpeggio::ascend(s1.clone() - Oct, 10)
+            Arpeggio::custom_order(
+                s1.clone() + Oct,
+                10,
+                vec![0, 1, 2, 3, 4, 1, 4, 0, 1, 2, 3, 4, 0, 3]
+            ),
+            Arpeggio::descend(s1.clone(), 20)
         ]
     ).unwrap()
 }
