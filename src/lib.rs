70976fd96bc76c358ac7ad88e67d5da08d83f021
Alex Pagan <alexrpagan@users.noreply.github.com>
2023-07-23 04:46:50 -0400

    Add more involved example

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use midi::{Midi};
+use crate::dropout::Dropout;
 
 pub mod sequences;
 pub mod router;
+pub mod dropout;
 pub mod drumlogue;
 pub mod rand;
 pub mod arp;
@@ -14,4 +16,30 @@ pub mod tone;
 
 pub trait Midibox {
     fn next(&mut self) -> Option<Vec<Midi>>;
+    fn name(&self) -> Option<&str> {
+        None
+    }
+}
+
+pub struct NamedMidibox {
+    name: String,
+    delegate: Box<dyn Midibox>
+}
+
+impl NamedMidibox {
+    fn wrap(name: &str, delegate: Box<dyn Midibox>) -> Box<dyn Midibox>{
+        return Box::new(NamedMidibox {
+          name: name.to_string(), delegate
+        })
+    }
+}
+
+impl Midibox for NamedMidibox {
+    fn next(&mut self) -> Option<Vec<Midi>> {
+        return self.delegate.next();
+    }
+
+    fn name(&self) -> Option<&str> {
+        Some(&self.name)
+    }
 }
