daf8980d1c47154e7f9386eb224cbfb7241f3e6a
Alex Pagan <alexrpagan@users.noreply.github.com>
2023-07-23 04:46:50 -0400

    Add more involved example

---
+++ src/dropout.rs
@@ -0,0 +1,47 @@
+use crate::Midibox;
+use rand::Rng;
+use crate::chord::Chord;
+use crate::midi::{Midi, MutMidi};
+use crate::tone::Tone;
+
+pub struct Dropout {
+    duration: u32,
+    duration_seen: u32,
+    playing: bool,
+    midibox: Box<dyn Midibox>,
+}
+
+impl Dropout {
+    pub fn wrap(midibox: Box<dyn Midibox>, duration: u32, playing: bool) -> Box<dyn Midibox> {
+        Box::new(Dropout {
+            duration,
+            playing,
+            duration_seen: 0,
+            midibox
+        })
+    }
+}
+
+impl Midibox for Dropout {
+    fn next(&mut self) -> Option<Vec<Midi>> {
+        if self.duration_seen >= self.duration {
+            self.duration_seen = 0;
+            self.playing = !self.playing;
+        }
+        let to_play: Option<Vec<Midi>> = self.midibox.next();
+        return match to_play {
+            Some(notes) => {
+                let to_play_chord = Chord { notes: notes.clone() };
+                self.duration_seen += to_play_chord.total_duration();
+                if self.playing {
+                    // forward the notes
+                    return Some(notes)
+                } else {
+                    // otherwise take a rest
+                    return Some(notes.iter().map(|n| n.set_pitch(Tone::Rest, 0)).collect())
+                }
+            }
+            None => None
+        };
+    }
+}
