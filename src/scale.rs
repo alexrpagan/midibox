c453a70173e4ff7c6d527e16eb9098ac1082822a
Alex Pagan <alexrpagan@users.noreply.github.com>
2023-07-29 20:41:10 -0400

    Plane tune

--- src/scale.rs
+++ src/scale.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+use crate::chord::Chord;
 use crate::midi::{Midi};
 use crate::tone::Tone;
 
@@ -36,6 +38,49 @@ impl Scale {
             ))
         }
         midi
+    }
+
+    pub fn make_chord(&self, oct: u8, degree: Degree, pitches: &Vec<Pitch>) -> Option<Chord> {
+        let scale_root = Midi::from_tone(self.root, oct);
+        match self.harmonize_up(scale_root, degree) {
+            None => None,
+            Some(chord_root) => {
+                let notes = pitches.iter().map(|chord_pitch| match chord_pitch {
+                    Pitch::Harmonize(degree, direction) => {
+                        let harmd = if direction.is_up() {
+                            self.harmonize_up(chord_root, degree.clone())
+                        } else {
+                            self.harmonize_down(chord_root, degree.clone())
+                        };
+                        harmd.map(|n|
+                            n.set_pitch(
+                                n.tone,
+                                (n.oct as i8 + direction.octaves_to_move()) as u8
+                            )
+                        )
+                    }
+                    Pitch::Transpose(interval, direction) => {
+                        let posed = if direction.is_up() {
+                            chord_root.transpose_up(interval.clone())
+                        } else {
+                            chord_root.transpose_down( interval.clone())
+                        };
+                        Some(posed.set_pitch(
+                            posed.tone,
+                            (posed.oct as i8 + direction.octaves_to_move()) as u8)
+                        )
+                    }
+                });
+                let mut chord_notes: Vec<Midi> = vec![];
+                for note in notes {
+                    match note {
+                        None => return None,
+                        Some(n) => chord_notes.push(n)
+                    }
+                }
+                return Some(Chord::new(chord_notes))
+            }
+        }
     }
 
     pub fn harmonize_up(&self, midi: Midi, harmonize: Degree) -> Option<Midi> {
@@ -88,6 +133,38 @@ impl Scale {
     }
 }
 
+pub enum Pitch {
+    Harmonize(Degree, Direction),
+    Transpose(Interval, Direction)
+}
+
+pub enum Direction {
+    Up,
+    Down,
+    UpShiftOct(i8),
+    DownShiftOct(i8)
+}
+
+impl Direction {
+    fn is_up(&self) -> bool {
+        match self {
+            Direction::Up => true,
+            Direction::Down => false,
+            Direction::UpShiftOct(_) => true,
+            Direction::DownShiftOct(_) => false
+        }
+    }
+
+    fn octaves_to_move(&self) -> i8 {
+        match self {
+            Direction::Up => 0,
+            Direction::Down => 0,
+            Direction::UpShiftOct(o) => *o as i8,
+            Direction::DownShiftOct(o) => -(*o as i8)
+        }
+    }
+}
+
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Degree {
@@ -103,7 +180,7 @@ pub enum Degree {
     Tenth,
     Eleventh,
     Twelveth,
-    Thirteenth,
+    Thirteenth
 }
 
 impl Degree {
