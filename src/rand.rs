e74b2e8da33f93a218671d9e5f9f47661bd6f075
Alex Pagan <alexrpagan@users.noreply.github.com>
2023-07-29 22:32:45 -0400

    Clean up random velocity

--- src/rand.rs
+++ src/rand.rs
@@ -1,32 +1,11 @@
-use crate::Midibox;
+use crate::{Map, Midibox};
 use rand::Rng;
 use crate::midi::Midi;
 
-
-pub struct RandomVelocity {
-    factor: f64,
-    midibox: Box<dyn Midibox>,
-}
-
-impl RandomVelocity {
-    pub fn wrap(midibox: Box<dyn Midibox>) -> Box<dyn Midibox> {
-        Box::new(RandomVelocity {
-            factor: 1_f64,
-            midibox
-        })
-    }
-}
-
-impl Midibox for RandomVelocity {
-    fn next(&mut self) -> Option<Vec<Midi>> {
+pub fn random_velocity(midibox: Box<dyn Midibox>) -> Box<dyn Midibox> {
+    Map::wrap(midibox, |m| {
         let v = rand::thread_rng().gen_range(0..99);
-        self.factor = (v as f64) / (100_f64);
-        self.midibox.next()
-            .map(|it|
-                it.into_iter()
-                    .map(|note| {
-                        note.set_velocity((note.velocity as f64 * self.factor) as u8)
-                    }).collect::<Vec<Midi>>()
-            )
-    }
+        let factor = (v as f64) / (100_f64);
+        m.set_velocity((m.velocity as f64 * factor) as u8)
+    })
 }
